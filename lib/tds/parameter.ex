df7eae16d2e7b8f1a27d9294a95047b8ec42750d
Joseph Bianchi <joe@funnelcloud.io>
2023-10-03 17:10:08 -0400

    test

--- lib/tds/parameter.ex
+++ lib/tds/parameter.ex
@@ -80,7 +80,7 @@ defmodule Tds.Parameter do
   def fix_data_type(%__MODULE__{type: nil, value: nil} = param) do
     # should fix ecto has_one, on_change :nulify issue where type is not know when ecto
     # build query/statement for on_chage callback
-    %{param | type: :binary}
+    %{param | type: :null}
   end
 
   def fix_data_type(%__MODULE__{value: value} = param)
